001598a557d7fe4a4c49cba227b3f1207a0420a1
Memory_Leak <irealing@163.com>
2019-04-22 11:13:41 +0800

    feat: 修改配置文件名称

--- src/main/kotlin/cn/fuser/amap/Config.kt
+++ src/main/kotlin/cn/fuser/amap/Config.kt
@@ -2,7 +2,9 @@ package cn.fuser.amap
 
 import cn.fuser.util.Property
 
+private const val defaultConfigFile = "amap.properties"
+
 object Config {
-    private val prop = Property.create("config.properties")
-    val key:String by prop.string("amap.key")
+    private val prop = Property.create(defaultConfigFile)
+    val key: String by prop.string("amap.key")
 }
